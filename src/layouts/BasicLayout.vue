7bb08e4faf0150ab816b95685005bba8e089531b
scc <1174821561@qq.com>
2025-03-04 22:33:40 +0800

    feat: 添加导航栏及路由配置

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="basicLayout">
+    <a-layout>
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer"
+        ><a href="https://baidu.com" target="_blank">去百度啦</a></a-layout-footer
+      >
+    </a-layout>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+
+<style scoped>
+  #basicLayout .footer {
+    background: gray;
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    padding: 16px;
+  }
+
+  #basicLayout .content {
+    padding: 20px;
+    margin-bottom: 20px;
+    background: linear-gradient(to right, #fefefe, #fff);
+  }
+
+  #basicLayout .header {
+    background: white;
+    margin-bottom: 16px;
+    color: unset;
+    padding-inline: 20px;
+  }
+</style>
